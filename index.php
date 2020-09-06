750d4c942d115d7d3686bdab16243a2149b52b30
LH XoomCoder <github@xoomcoder.com>
2020-09-06 14:03:31 +0200

    add css et js

--- index.php
+++ index.php
@@ -2,3 +2,5 @@
 
 // add autoload from composer
 require __DIR__ . "/vendor/autoload.php";
+
+require __DIR__ . "/php/templates/index.php";
---
+++ php/templates/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>XoomCoder App</title>
+
+    <link rel="stylesheet" href="assets/css/site.css">
+</head>
+<body>
+    <div class="page">
+        <header>
+            <h1>SITE TITLE</h1>
+        </header>
+        <main>
+            <section>
+                <h1>TITLE1 SECTION</h1>
+            </section>
+        </main>
+        <footer>
+            <p>all rights reserved - © 2020</p>
+        </footer>
+    </div>
+
+    <script type="module" src="assets/js/site.js"></script>
+</body>
+</html>
